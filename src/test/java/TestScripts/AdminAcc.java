c5ab5dc9cafd6fe30ea22ec7a48191cbcdfe8feb
Ajay Pandita <ajay4evr.ap@gmail.com>
2025-04-17 17:57:07 +0530

    log4j implementation Ajay

--- src/test/java/TestScripts/AdminAcc.java
+++ src/test/java/TestScripts/AdminAcc.java
@@ -4,6 +4,8 @@ import org.testng.annotations.Test;
 import org.testng.annotations.Test;
 import java.time.Duration;
 
+import org.apache.logging.log4j.LogManager;
+import org.apache.logging.log4j.Logger;
 import org.openqa.selenium.By;
 import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.support.ui.ExpectedConditions;
@@ -22,24 +24,30 @@ import UtilityPackage.listnerClass;
 import UtilityPackage.propartieClass;
 @Listeners(listnerClass.class)
 public class AdminAcc extends BaseClass{
-
+    private static final Logger logger = LogManager.getLogger(AdminAcc.class);
 	HomepagePOM home;
 	AdminPOM admin;
 	
 	@Test(priority = 0)
 public void login() throws Exception {
-		
-		
+		logger.info("Window Maximized");
+		logger.trace("This is a TRACE log");
+        logger.debug("This is a DEBUG log");
+        logger.info("This is an INFO log");
+        logger.warn("This is a WARN log");
+        logger.error("This is an ERROR log");
 		propartieClass pro = new propartieClass();
 		String name=pr.proparties("username");
-		String pass=pr.proparties("password");	
-		
+		String pass=pr.proparties("password");		
 		loginPOM pom=new loginPOM(driver);
+
 		WebDriverWait wait = new WebDriverWait(driver, Duration.ofSeconds(10));
 		wait.until(ExpectedConditions.visibilityOf(pom.username()));
 		pom.username().sendKeys(name);
 		pom.password().sendKeys(pass);
 		pom.button().click();
+		logger.info("Now Logging IN Orange HRM");
+
 }
 	
 	@Test(priority = 1)
@@ -50,12 +58,18 @@ public void login() throws Exception {
 		String actual_result =driver.getCurrentUrl();
 		if(expected_result.equals(actual_result)) {
 			System.out.println("I am homepage");
-		
+			
+
 		pim_POM pim=new pim_POM(driver);
 		home  = new HomepagePOM(driver);
 		wait.until(ExpectedConditions.elementToBeClickable(home.PIM()));
 		home.PIM().click();
+		
+		logger.debug("Now Clicking on PIM module");
+
 		pim.addEmp();
+		
+		logger.info("Adding Employee Details");
 		wait.until(ExpectedConditions.visibilityOf(pim.firstname()));
 		pim.firstname().sendKeys(excelUtil.data("Sheet1", 7, 1));
 		pim.lastName().sendKeys(excelUtil.data("Sheet1", 8, 1));
